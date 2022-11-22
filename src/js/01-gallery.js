49adf0afe71ccbe056f84fd685d8d4f8800794e8
maks-sova <107675543+maks-sova@users.noreply.github.com>
2022-11-22 02:28:29 +0200

    +

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,11 +1,11 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
-console.log(galleryItems);
+import { galleryItems } from "./gallery-items.js";
 
+const refs = {
+  imageContainer: document.querySelector(".gallery"),
+  body: document.body,
+};
 
-const divref = document.querySelector('.gallery');
-
-function makeGalleryItems(items) {
+function makegalleryItems(items) {
   return items
     .map(({ preview, description, original }) => {
       return `<div class="gallery__item">
@@ -22,32 +22,61 @@ function makeGalleryItems(items) {
     .join("");
 }
 
-const addGallaryMarkup = makeGalleryItems(galleryItems);
+const cardgalleryMarkup = makegalleryItems(galleryItems);
+refs.imageContainer.insertAdjacentHTML("beforeend", cardgalleryMarkup);
 
-divref.innerHTML = addGallaryMarkup;
-
-divref.addEventListener('click', onImageClick);
-
-function onImageClick(evt) {
-
-    blockStandartAction(evt);
-
-    if (evt.target.nodeName !== "IMG") {
-        return;
+const createModalWindow = (imageAdress) => {
+  window.instance = basicLightbox.create(
+    `
+    <img src="${imageAdress}">
+`,
+    {
+      onShow: () =>
+        window.addEventListener("keydown", closeModalWindowByEscPressing),
+      onClose: () => {
+        window.removeEventListener("keydown", closeModalWindowByEscPressing);
+        refs.body.classList.remove("disable-scroll");
+      },
     }
+  );
+  return instance;
+};
 
-const instance = basiclightbox.create(`
-    <img src="${evt.target.dataset.sourse}" width="400" heigth="300">
-`);
-instance.show();
+refs.imageContainer.addEventListener("click", onClickOpenModal);
 
-divref.addEventListener('keydown', (evt) => {
-    if (evt.code === "Escape") {
-        instance.close()
-    }
-});
+function onClickOpenModal(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const originalImageRef = event.target.dataset.source;
+  createModalWindow(originalImageRef).show();
+  refs.body.classList.add("disable-scroll");
 }
 
-function blockStandartAction(evt) {
-    evt.preventDefaut();
+function closeModalWindowByEscPressing(event) {
+  const ESC_KEY_CODE = "Escape";
+  if (event.code === ESC_KEY_CODE && instance.visible()) {
+    instance.close();
+    refs.body.classList.remove("disable-scroll");
+  }
+}
+
+const lazyImages = refs.imageContainer.querySelectorAll(".gallery__image");
+
+lazyImages.forEach((image) =>
+  image.addEventListener("load", onImageLoaded, { once: true })
+);
+
+function onImageLoaded(event) {
+  event.target.classList.add("appear");
+}
+
+lazyImages.forEach((image) =>
+  image.addEventListener("mouseenter", onMouseEnter)
+);
+
+function onMouseEnter(event) {
+  event.target.style.transitionDelay = "100ms";
+  event.target.style.transitionDuration = "500ms";
 }
